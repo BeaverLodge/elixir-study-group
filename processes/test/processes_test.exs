0fe4b8388be08de45915db73798a54182dd08003
Martin Feckie <mfeckie@gmail.com>
2016-03-08 15:09:49 +0800

    Add tests for process based greeter

--- processes/test/processes_test.exs
+++ processes/test/processes_test.exs
@@ -2,7 +2,28 @@ defmodule ProcessesTest do
   use ExUnit.Case
   doctest Processes
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Sending greeter a message" do
+    greeter_pid = Processes.greeter
+    send greeter_pid, {self, "World"}
+
+    assert_receive {:ok, "Hello, World"}
+
+    send greeter_pid, {self, "Dave!"}
+
+    assert_receive {:ok, "Hello, Dave!"}
   end
+
+  @tag :skip
+  test "Counting the number of requests" do
+    count_greet = Processes.count_greeter
+
+    send count_greet, {self, "World!"}
+
+    assert_receive {:ok, "Hello, World! count: 1"}
+
+    send count_greet, {self, "World!"}
+
+    assert_receive {:ok, "Hello, World! count: 2"}
+  end
+
 end
