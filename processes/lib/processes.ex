0b99dfccf46891381633eed3739c6b3111379d7b
Martin Feckie <mfeckie@gmail.com>
2016-03-08 15:10:59 +0800

    Add baseline implementation for greeter as an example

--- processes/lib/processes.ex
+++ processes/lib/processes.ex
@@ -1,2 +1,34 @@
 defmodule Processes do
+  @module """
+  This module is to demonstrate the use of processes in Elixir / Erlang.
+
+  Implement the necessary functions to make the tests pass.
+
+  The greeter is provided as an example to get things going.
+  """
+
+  @doc """
+  greeter returns a process id (PID)
+
+  iex> pid = Processes.greeter
+  iex> is_pid(pid)
+  true
+  """
+  def greeter do
+    spawn(Processes, :greet, [])
+  end
+
+
+  @doc """
+  greet responds to messages with Hello + name
+
+  This function recursively invokes itself to remain alive.
+  """
+  def greet do
+    receive do
+      {sender, msg} ->
+        send sender, {:ok, "Hello, #{msg}"}
+        greet
+    end
+  end
 end
